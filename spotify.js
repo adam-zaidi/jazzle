101188ef64774d7bdd63a562abe0d8e9dacd1303
adam-zaidi <57157067+adam-zaidi@users.noreply.github.com>
2024-04-30 18:24:59 +0000

    finished everything but spotify api

--- spotify.js
+++ spotify.js
@@ -1,16 +1,31 @@
-// function callToken() {
-//     let parameters = ["client_id" : "3f003b5880f64a0ca6e6e4276f31339b",// u get in developer account in Spotify.
-//                       "client_secret" : "be52b966c4e4421fbdac923356844170",
-//                       "grant_type" : "client_credentials"]
-//     Alamofire.request("https://accounts.spotify.com/api/token", method: .post, parameters: parameters).responseJSON(completionHandler: {
-//         response in
-//         print(response)
-//         print(response.result)
-//         print(response.result.value)
-//         if let result = response.result.value {
-//             let jsonData = result as! NSDictionary
-//             let token = jsonData.value(forKey: "access_token") as? String
-//             print(token!)
-//           }
-// })
-// } 
+const token = "BQBNKPKvaiiZ2amfSYFXIKR3FN2pXssKvpanfFLKl5-2-p-R6kTdEnLA---9d0nVe2xt_hMFD-5cLjI8ZR6-aKQLWtsDPWFzpfIVi25nUPbQKptEXWs";
+
+async function fetchWebApi(endpoint, method, body) {
+  const res = await fetch(`https://api.spotify.com/${endpoint}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    method,
+    body:JSON.stringify(body)
+  });
+  return await res.json();
+}
+
+const topTracksIds = [
+  '1I3HTXuNYqmDYWywwf2v9h','4rPv8eZH6ABfkrtxxHHtV4','7wjmwD5nIYWVnHiR3X3PTO','0HWWt7Nv69TSuaVc5Woy7O','620q3OKfZXyy4dstdxSi9K'
+];
+
+async function getRecommendations(){
+  // Endpoint reference : https://developer.spotify.com/documentation/web-api/reference/get-recommendations
+  return (await fetchWebApi(
+    `v1/recommendations?limit=5&seed_tracks=${topTracksIds.join(',')}`, 'GET'
+  )).tracks;
+}
+
+const recommendedTracks = await getRecommendations();
+console.log(
+  recommendedTracks.map(
+    ({name, artists}) =>
+      `${name} by ${artists.map(artist => artist.name).join(', ')}`
+  )
+);
